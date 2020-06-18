7efcbdff2a0fd985254a0ae0f8dcbb89e23982df
Alex Blumenthaler <blumenthaleralex@gmail.com>
2020-06-18 17:22:48 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,9 +3,9 @@ def speak_to_grandma(string)
   if string != string.upcase
     return "HUH?! SPEAK UP, SONNY!"
   elsif string.upcase && string != "I LOVE YOU GRANDMA!"
+    return "NO, NOT SINCE 1938!"
+  elsif string.upcase && string = "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif string.upcase
-    return 
  end
 end
 
