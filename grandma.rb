1548829797e4125f82d55dc513c06f227797fff9
Alex Blumenthaler <blumenthaleralex@gmail.com>
2020-06-18 17:14:44 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,8 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(string)
-  if string = !string.upcase
+  if string == !string.upcase
      "HUH?! SPEAK UP, SONNY!"
-  elsif string = string.upcase && string != "I LOVE YOU GRANDMA!"
+  elsif string == string.upcase && string !== "I LOVE YOU GRANDMA!"
      "NO, NOT SINCE 1938!"
   elsif string = "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
