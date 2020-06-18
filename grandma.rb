340af9f6a52f8da51849a215d77dad036589e6a2
Alex Blumenthaler <blumenthaleralex@gmail.com>
2020-06-18 17:11:00 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(string)
-  if string.downcase
+  if !string.upcase
      "HUH?! SPEAK UP, SONNY!"
   elsif string.upcase && !string == "I LOVE YOU GRANDMA!"
      "NO, NOT SINCE 1938!"
